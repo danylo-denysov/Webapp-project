091d313da08a6e683bf22c5128773389cefb82e4
Danylo Denysov <danylo-denysov>
2025-03-28 15:06:56 +0100

    some example data validations

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Injectable()
 export class TasksService {
@@ -31,7 +32,12 @@ export class TasksService {
   }
 
   get_task_by_id(id: string): Task {
-    return this.tasks.find((task) => task.id === id)!; // assert that return param is not null
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`); // 404 response
+    }
+
+    return found; // assert that return param is not null
   }
 
   create_task(createTaskDto: CreateTaskDto): Task {
@@ -52,8 +58,9 @@ export class TasksService {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  update_task_status(id: string, status: TaskStatus) {
+  update_task_status(id: string, updateTaskStatusDto: UpdateTaskStatusDto) {
     const task = this.get_task_by_id(id);
+    const { status } = updateTaskStatusDto;
     task.status = status;
     return task;
   }
