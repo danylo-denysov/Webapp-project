8c1374268cc32e45e641570774211d9327bf0a14
Danylo Denysov <danylo-denysov>
2025-06-02 22:59:57 +0200

    fix(docs): fix main.ts

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -6,35 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Task Management API')
-    .setDescription(
-      'Dokumentacja REST API do zarządzania tablicami i zadaniami',
-    )
-    .setVersion('1.0.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'Authorization',
-        description: 'Wprowadź JWT w formacie: Bearer <token>',
-        in: 'header',
-      },
-      'jwt-auth', // key
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config, {
-    // exclude: [InternalModule],
-  });
-
-  SwaggerModule.setup('api/docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
-
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -42,6 +13,25 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe()); // Automatically validate incoming requests based on DTOs
   app.setGlobalPrefix('api'); // Set a global prefix for all routes
   app.use(cookieParser()); // Use cookie parser middleware to handle cookies from http headers
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('REST API for boards, task‐groups, tasks and users')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token', // This name ('access-token') is how we reference the security scheme in @ApiSecurity/@ApiBearerAuth
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.startAllMicroservices(); // Start all microservices
 
