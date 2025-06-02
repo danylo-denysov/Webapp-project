8ebe39a788811f2f2a7338364963432d8f43b167
Danylo Denysov <danylo-denysov>
2025-06-02 22:47:24 +0200

    feat(docs): swagger config

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -2,9 +2,39 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription(
+      'Dokumentacja REST API do zarządzania tablicami i zadaniami',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Wprowadź JWT w formacie: Bearer <token>',
+        in: 'header',
+      },
+      'jwt-auth', // key
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    // exclude: [InternalModule],
+  });
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
